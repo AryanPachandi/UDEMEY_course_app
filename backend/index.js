48f875908acaea7296e31b99ef60d206e5f0acb4
Aryan Pachandi <142322888+AryanPachandi@users.noreply.github.com>
2025-03-16 11:42:08 +0530

    completed backend
    # Conflicts:
    #	backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const {adminRouter} = require('./routes/admin.js');
 const {courseRouter} = require('./routes/course.js');
 const {authRouter} = require('./routes/auth.js');
 
+require('dotenv').config();
 
 app.use(express.json());
 
@@ -18,7 +19,14 @@ app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/auth", authRouter);
 
 async function main() {
+<<<<<<< HEAD
     await mongoose.connect("" );
+=======
+    await mongoose.connect(process.env.MONGO_URI, )
+        .then(() => console.log("MongoDB Connected"))
+        .catch(err => console.log(err));
+
+>>>>>>> 81209e4 (completed backend)
     app.listen(3000);
     console.log("App is running");
 }
